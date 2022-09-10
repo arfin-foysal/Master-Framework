40645741b1c1a54aaeb956c0dca2073a8c78d4a3
Arfin Foysal <arfinfoysalbd@gmail.com>
2022-09-10 23:59:30 +0600

    deploy

--- app.js
+++ app.js
@@ -42,30 +42,30 @@ app.use('/api/v1/rolepermissions', rolePermissionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  // next(createError(404));
-  next(createError(404, 'Requested resource not found!'));
+  next(createError(404));
+  // next(createError(404, 'Requested resource not found!'));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 
-  if(res.headersSent){
-    return next("There was an problem with the server!");
-  } else {
-    if(err.message){
-      res.status(500).send(err.message);
-    }
-    else {
-      res.send("There was an error!");
-    }
-  }
+  // if(res.headersSent){
+  //   return next("There was an problem with the server!");
+  // } else {
+  //   if(err.message){
+  //     res.status(500).send(err.message);
+  //   }
+  //   else {
+  //     res.send("There was an error!");
+  //   }
+  // }
 
 });
 
